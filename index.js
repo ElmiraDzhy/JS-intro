115fe03ffad5f9a61a3ef068f4f39c7f687e9699
ElyaDzhy <elmira.freshcode@gmail.com>
2023-03-14 09:49:42 +0200

    type conversion example

---
+++ index.js
@@ -0,0 +1,33 @@
+console.log(typeof 'str');
+console.log(typeof true);
+console.log(typeof 1);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+console.log(!false);
+console.log(!NaN);
+console.log(!null);
+
+/*
+string
+boolean
+number
+object
+undefined
+
+
+true
+true
+true
+*/
+
+console.log(!!'1');
+console.log(!!NaN);
+console.log(!!null);
+
+/**
+ * true
+ * false
+ * false
+ */
