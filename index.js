d43a35cb8faee61d5d49439c3c592ea21250704b
ElyaDzhy <elmira.freshcode@gmail.com>
2023-03-15 11:09:09 +0200

    Add example of functions

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 'use strict';
 
-let age = 18;
-let result; 
+
+let userAge = Number(prompt());
+
+//
+function isAdult (userAge){
+    return userAge >= 18 ? true : false;
+}
+
+console.log(`${isAdult(userAge) ? 'user is adult' : 'user is not adult' }`);
+
+//
+
+function mySum (a, b){
+    return a +b;
+}
 
 
-age >= 18 ? result = 'adult' :  result = 'not adult';
+console.log(mySum(5,16));
+
+//
+
+const divElem = document.createElement('div');
+document.body.appendChild(divElem);
+divElem.style.backgroundColor = 'blue';
+divElem.style.height = '100px';
+divElem.style.width = '100px';
+divElem.textContent = 'Text from js.file';
+divElem.style.color = 'white';
+
+//
+
+function argFun(...arg){
+    return arg.map((item) => item*item);
+}
 
 
-console.log(result);
+
+
+
+
+
