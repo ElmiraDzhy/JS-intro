8e5b09bd543c19f0dcd3d0ef022fab4c78cddf91
ElyaDzhy <elmira.freshcode@gmail.com>
2023-03-15 12:15:14 +0200

    Add homework task

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 'use strict';
 
 
-function getBiggerNum (a = Number(prompt()), b = Number(prompt())){
-    return a > b ? a : b;
+function toSquare(...args){
+    return args.map( item => item*item );
 }
 
-function sum2 (...args){
-    let result = 0;
-    for(let i = 0; i < args.length; i++){
-        result += args[i];
-    }
-    return result;
+const toSquare2 = (...args) => args.map( item => item*item )
+
+
+function threeNum (a, b, c){
+    return a*b/c;
 }
 
-function isEven (num){
-    return num % 2 ? false : true;
-    // return !Boolean(num % 2);
+const threeNum2 = (a, b, c) =>  a*b/c;
+
+function greeting (str){
+    return `Hello, ${str}!`;
 }
 
-const isEven2 = num =>  num % 2 === 0 ? true : false;
-
-
-
-
+const greeting2 = str => `Hello, ${str}`;
 
